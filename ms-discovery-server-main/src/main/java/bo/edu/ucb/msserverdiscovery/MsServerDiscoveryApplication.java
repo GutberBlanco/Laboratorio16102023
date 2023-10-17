4047f6d42206cc381c19d836482b1f94d5e6b2da
GutberBlanco <100219714+GutberBlanco@users.noreply.github.com>
2023-10-16 20:56:49 -0400

    Add files via upload

---
+++ ms-discovery-server-main/src/main/java/bo/edu/ucb/msserverdiscovery/MsServerDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package bo.edu.ucb.msserverdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsServerDiscoveryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsServerDiscoveryApplication.class, args);
+	}
+
+}
---
+++ ms-discovery-server-main/src/test/java/bo/edu/ucb/msserverdiscovery/MsServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.msserverdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
